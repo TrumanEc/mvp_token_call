66506fea365f1e65560849dd267646497b4aebb1
Darwin Esteban Cifuentes G <tecnicoesteban01@gmail.com>
2026-03-03 10:36:31 -0500

    clean: remove next cache from tracking

--- .next/dev/server/app/api/marketplace/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/marketplace/route.js")
-R.c("server/chunks/[root-of-the-server]__7b6981c3._.js")
-R.c("server/chunks/[root-of-the-server]__3e25dee9._.js")
-R.c("server/chunks/_next-internal_server_app_api_marketplace_route_actions_d03a1415.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/marketplace/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/marketplace/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .next/dev/server/app/api/markets/[id]/price-quote/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../../../chunks/[turbopack]_runtime.js")("server/app/api/markets/[id]/price-quote/route.js")
-R.c("server/chunks/[root-of-the-server]__ad6464fb._.js")
-R.c("server/chunks/[root-of-the-server]__e06aab95._.js")
-R.c("server/chunks/_next-internal_server_app_api_markets_[id]_price-quote_route_actions_0ab70057.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/markets/[id]/price-quote/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/markets/[id]/price-quote/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
